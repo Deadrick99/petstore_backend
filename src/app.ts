67f474127aa958e837744e72bd05a18b2f4b8d00
Dante <delariva@etsu.edu>
2022-09-07 19:02:37 -0400

    Implement test express api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+import express from "express";
+const app = express();
+
+const PORT = 3000;
+
+// Tell express to parse request body as json
+app.use(express.json());
+
+// Routes
+app.get("/api/test", (req, res) => {
+  res.send({ test: "success" });
+});
+
+// Listen on port
+app.listen(PORT, () => console.log(`Server starting on port ${PORT} ...`));
